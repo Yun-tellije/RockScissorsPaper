c464e0ad069e00d20fbd2933fa2acbfae8afdac6
jeun0517 <dbswjddms517@naver.com>
2023-06-23 20:42:47 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class user{
+    int score;
+    String hand;
+
+    user(){
+        score = 0;
+        hand = null;
+    }
+
+}
+
+class computer{
+    int score;
+    String hand;
+
+    computer(){
+        score = 0;
+        hand = null;
+    }
+}
+
+class total{
+
+}
+
+class Game_Control{
+
+    public void run(){
+
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        Game_Control gamecontrol = new Game_Control();
+        gamecontrol.run();
+    }
+}
